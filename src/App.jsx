5fea16d525450a6a30d9c1981ce7a63f6f2efe1d
Ed Dowding <me@eddowding.com>
2025-01-03 13:55:42 +0000

    Initial commit from your app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChatInterface from './components/ChatInterface'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <main className="container mx-auto px-4 py-8">
+        <ChatInterface />
+      </main>
+    </div>
+  )
+}
+
+export default App
